df18451a8996fc105a943850e96f888e14d693b0
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-16 10:05:36 +0530

    16-03-2022

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello everyone")
+// v=3 
+// console.log("Hello ")
+
+// v=4
+// REPL 
+// 1. js Expression
+//2. use variable 
+//3.Multiine code / loop
+//4.use(_) to get the last result
+//5.we can use editor mode
+
+// v=4
+// FS Module
+ 
+const fs = require('fs')
+fs.writeFileSync("read.txt","Welcome to my chanccel")
