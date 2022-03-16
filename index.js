d853a5fe81e60ffb7d60cd767ce7ae0bdb2b997c
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-16 10:51:09 +0530

    16-03-2022

--- index.js
+++ index.js
@@ -12,5 +12,24 @@
 // v=4
 // FS Module
  
-const fs = require('fs')
-fs.writeFileSync("read.txt","Welcome to my chanccel")
+const fs = require('fs')       
+
+// create a new file 
+// fs.writeFileSync("read.txt","Welcome to my chanccel")    
+
+// file in add content
+// fs.appendFileSync("read.txt","Hello How are you I am fine")
+
+// read a file 
+// const data=fs.readFileSync("read.txt")
+// org_data=data.toString()
+// console.log(org_data)
+ 
+// Node.js includes an additional data type called Buffer
+//(note  availabele in broweser's javascript)
+//Buffer is mainly used to store binary data,
+//while reading  from  a file  or receiving  packets over the network
+
+// to reaname the file
+
+fs.renameSync("read.txt","readwrite.txt")
