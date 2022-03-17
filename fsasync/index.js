4c637e3956ddf7ee9123e4e6e34bf56dc760fd84
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-17 10:28:57 +0530

    17-03-2022

--- fsasync/index.js
+++ fsasync/index.js
@@ -1,4 +1,11 @@
 const fs= require('fs');
-fs.writeFile("gg.txt","today is awesome day :","utf8",function(err,data){
-    console.log(err )
+
+// data write 
+// fs.writeFile("gg.txt","today is awesome day :","utf8",function(err,data){
+//     console.log(err )
+// })
+
+// add data 
+fs.appendFile("gg.txt","plz link share and sunds  my chanel ",(err)=>{
+    console.log(err)
 })
