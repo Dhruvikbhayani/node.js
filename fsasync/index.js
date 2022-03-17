03d2312da4d689dba74d5d7221b02534df88160a
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-17 10:33:17 +0530

    17-03-2022

--- fsasync/index.js
+++ fsasync/index.js
@@ -6,6 +6,12 @@ const fs= require('fs');
 // })
 
 // add data 
-fs.appendFile("gg.txt","plz link share and sunds  my chanel ",(err)=>{
-    console.log(err)
+// fs.appendFile("gg.txt","plz link share and sunds  my chanel ",(err)=>{
+//     console.log(err)
+// })
+
+// read file 
+fs.readFile("gg.txt","utf8",(err,data)=>{
+ console.log(err)
+console.log(data)
 })
