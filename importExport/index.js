6d8d1b15b4a28b399c39dbc47b563448339830d0
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-21 12:23:12 +0530

    21-03-2022

---
+++ importExport/index.js
@@ -0,0 +1,8 @@
+const {add,sub,multi,name}=require('./oper')
+
+// const a=require('./oper')
+console.log(add(5,3))
+console.log(sub(5,3))
+console.log(multi(5,3))
+console.log(name)
+// console.log(add)
---
+++ importExport/oper.js
@@ -0,0 +1,21 @@
+const add=(a,b)=>{
+    return a+b;
+}
+const sub=(a,b)=>{
+    return a-b;
+}
+
+const multi=(a,b)=>{
+    return a*b;
+}
+
+const name="Dhruvik"
+
+// module.exports =name 
+
+// module.exports.add=add
+// module.exports.sub=sub
+// module.exports.multi=multi
+
+
+module.exports={add,sub,multi,name}
