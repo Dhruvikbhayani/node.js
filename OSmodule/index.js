9dadabe58fb4099cf307035d7a52f55179d9ea7f
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-21 11:06:46 +0530

    21-03-22

---
+++ OSmodule/index.js
@@ -0,0 +1,17 @@
+const os = require('os')
+
+  console.log(os.arch());
+  console.log(os.hostname());
+  console.log(os.platform());
+  console.log(os.tmpdir());
+  console.log(os.type())
+
+//   const freememory=os.freemem()
+//   console.log(freememory)
+//   console.log(`${freememory/1024/1024/1024}`);
+  
+  
+//   const totalmemroy=os.totalmem()
+//   console.log(`${totalmemroy/1024/1024/1024}`);
+
+//   direct show the meroy   =ctrl+shift+esc
