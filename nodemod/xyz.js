1b34216b41bad482854fc4022191d4dfd57f49bb
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-22 09:58:12 +0530

    22-03-2022

--- nodemod/xyz.js
+++ nodemod/xyz.js
@@ -1,3 +1,6 @@
-const chalk = require('chalk')
+// const chalk = require('chalk')
+// console.log(chalk.blue("Hello world"))
 
-console.log(chalk.blue("Hello world"))
+var validator = require('validator');
+const res=validator.isEmail("dk@dk.com")
+console.log(res)
