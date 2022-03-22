0616163ff15ad003eca4d0d6baac509f1a9190f1
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-22 11:07:25 +0530

    22-03-22

---
+++ modwareefun/index.js
@@ -0,0 +1,15 @@
+// (function(exports,require,module,_filename,_dirname){
+
+//     const  name = "Dhruvik"
+//     console.log(name)
+// })
+
+
+// IIFE 
+// (function(){
+//     var a="Dhruvik"
+//     console.log(a)
+// })()
+
+console.log(__filename)
+console.log(__dirname)
