ee573ba25f8b062c56192c856999a89601880ffe
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-24 09:57:39 +0530

    ૨૪-૦૩-૨૨

--- jsonNode/index.js
+++ jsonNode/index.js
@@ -3,17 +3,18 @@
 //format for storing and transporting data.
 //json is often used when data is sent from a server to a web page 
 
-const fs=require('fs')
-const biodata ={
-    name:"Dhruvik",
-    age:20,
-    channel:"Dhruvik Bhayani"
-}
-// console.log(biodata.name)
+const fs = require('fs')
+const biodata = {
+        name: "Dhruvik",
+        age: 20,
+        channel: "Dhruvik Bhayani"
+    }
+    // console.log(biodata.name)
 
-// convert to object
-// const objdata=JSON.parse(jsondata)
-// console.log(objdata)                                                      //{ name: 'Dhruvik', age: 20, channel: 'Dhruvik Bhayani' }
+const jsondata = JSON.stringify(biodata)
+    // convert to object
+const objdata = JSON.parse(jsondata)
+console.log(objdata) //{ name: 'Dhruvik', age: 20, channel: 'Dhruvik Bhayani' }
 
 // 1:convert to json 
 //2: dusre file me add karna he
@@ -21,17 +22,12 @@ const biodata ={
 //4:again convert back to js obj 
 //5:console.log
 
-const jsondata=JSON.stringify(biodata)
 // console.log(jsondata)                                                //{"name":"Dhruvik","age":20,"channel":"Dhruvik Bhayani"}
 // fs.writeFile("json1.json",jsondata,function(err){
 //     console.log("done")
 // })
-fs.readFile("json1.json","utf-8",(err,data)=>{
-    const orgdata=JSON.parse(data)
-    console.log(data)
-    console.log(orgdata)
-})
-
-
-
-
+// fs.readFile("json1.json","utf-8",(err,data)=>{
+//     const orgdata=JSON.parse(data)
+//     console.log(data)
+//     console.log(orgdata)
+// })
