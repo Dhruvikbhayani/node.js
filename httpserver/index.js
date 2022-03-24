dbecea6eb273ad9c2889986194cae4f6b6f3fb0a
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-24 11:19:10 +0530

    24-03-22

--- httpserver/index.js
+++ httpserver/index.js
@@ -8,22 +8,38 @@
 //If the response from the http server is supposed to displayed as HTML
 //you should include an HTTP header with the current content type.
 
-const http=require('http');
-const { type } = require('os');
-const server=http.createServer((req,res)=>{
+const http = require('http');
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+
+
+    const data = fs.readFileSync(`${__dirname}/userapi/useapi.json`, "utf8", )
+    const object = JSON.parse(data)
+
+
     // console.log(req.url)
-    if(req.url =="/"){
-      res.end("Hello for home page ");
-    }else if(req.url=="/about"){
-      res.end("Hello for about page ");
-    }else if (req.url=="/contact"){
-      res.write("Hello for contact page")
-      res.end();
-    }else{
-      res.writeHead(404,{"content-type":"text/html"})
-      res.end("<h1>404 error </h1>");
+    if (req.url == "/") {
+        res.end("Hello for home page ");
+    } else if (req.url == "/about") {
+        res.end("Hello for about page ");
+    } else if (req.url == "/contact") {
+        res.write("Hello for contact page")
+        res.end();
+    } else if (req.url == "/userapi") {
+        // fs.readFile(`${__dirname}/userapi/useapi.json`, "utf8", (err, data) => {
+        //         console.log(data)
+        //         const object = JSON.parse(data)
+        res.writeHead(200, { "content-type": "applications/json" })
+        res.end(object[0].name)
+            //         res.end()
+            // })
+
+    } else {
+        res.writeHead(404, { "content-type": "text/html" })
+        res.end("<h1>404 error </h1>");
     }
 });
-server.listen(5020,()=>{
+server.listen(5020, () => {
     console.log("port is run")
 })
