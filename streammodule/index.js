3688facc521d63153f1cdfbe1bbbc88b3731d632
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-25 13:05:27 +0530

    25-03

--- streammodule/index.js
+++ streammodule/index.js
@@ -9,11 +9,34 @@ const http = require('http')
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-    var fs = require('fs')
-    fs.readFile("input.txt", "utf8", (err, data) => {
-        if (err) console.log(err);
-        res.end(data.toString())
-    })
+    // fs.readFile("input.txt", "utf8", (err, data) => {
+    //         if (err) console.log(err);
+    //         res.end(data.toString())
+    //     })
+
+    // ------------------------------------------------------------------------------------------------------
+    // 2nd way 
+    //reading from streams
+    //create a readable stream
+    //handle streams events -> data,error,and end
+
+    //     const rstream = fs.createReadStream('input.txt')
+
+    //     rstream.on("data", (chundata) => {
+    //         res.write(chundata)
+    //     })
+    //     rstream.on("end", () => {
+    //         res.end()
+    //     })
+    //     rstream.on("error", (error) => {
+    //         console.log(error)
+    //         res.end("file not found")
+    //     })
+    // -----------------------------------------------------------------------------------------------------------
+    //pipes()
+    const rstream = fs.createReadStream('input.txt')
+    rstream.pipe(res)
+
 })
 server.listen(8000, () => {
     console.log("port is run")
