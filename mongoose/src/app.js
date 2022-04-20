ee7b779bb374e7054a9234b61f7cd4f695ed53ce
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-04-20 10:25:46 +0530

    20-04-22

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -22,58 +22,74 @@ const playschema = new mongoose.Schema({
         required: true
     }
 })
-
-//insert data 
-
 const playlist = new mongoose.model("playlist", playschema)
 
-// const data = new playlist({
-//     name: "node js",
-//     type: "backend",
-//     list: "no"
-// })
-// data.save().then(() => {
-//     console.log("document insert")
-// })
+// //insert data 
 
-const createdata = async() => {
-        try {
-            const data = new playlist({
-                name: "js",
-                type: "frontend",
-                list: "no"
-            })
-            const data1 = new playlist({
-                name: "mongodb",
-                type: "database",
-                list: "no"
-            })
-            const data2 = new playlist({
-                name: "mongoose",
-                type: "module",
-                list: "no"
-            })
-            const data3 = new playlist({
-                name: "express",
-                type: "backend",
-                list: "no"
-            })
-            const savedata = await playlist.insertMany([data, data1, data2, data3]);
-            console.log(savedata)
 
-        } catch (err) {
-            console.log(err)
-        }
-    }
-    // createdata()
+// // const data = new playlist({
+// //     name: "node js",
+// //     type: "backend",
+// //     list: "no"
+// // })
+// // data.save().then(() => {
+// //     console.log("document insert")
+// // })
+
+// const createdata = async() => {
+//         try {
+//             const data = new playlist({
+//                 name: "js",
+//                 type: "frontend",
+//                 list: "no"
+//             })
+//             const data1 = new playlist({
+//                 name: "mongodb",
+//                 type: "database",
+//                 list: "no"
+//             })
+//             const data2 = new playlist({
+//                 name: "mongoose",
+//                 type: "module",
+//                 list: "no"
+//             })
+//             const data3 = new playlist({
+//                 name: "express",
+//                 type: "backend",
+//                 list: "no"
+//             })
+//             const savedata = await playlist.insertMany([data, data1, data2, data3]);
+//             console.log(savedata)
+
+//         } catch (err) {
+//             console.log(err)
+//         }
+//     }
+// createdata()
 
 
 
 //read data 
+// const getdocument = async() => {
+//     const result = await playlist.find()
+//     console.log(result)
+// }
 
-const getdocument = async() => {
-    const result = await playlist.find()
-    console.log(result)
+// getdocument()
+
+//update  document
+
+const updateDocument = async(_id) => {
+    try {
+        const result = await playlist.findByIdAndUpdate({ _id }, {
+            $set: {
+                name: "node js"
+            }
+        })
+        console.log(result)
+    } catch (err) {
+        console.log(err)
+    }
 }
 
-getdocument()
+updateDocument('625a5aa758ac0a7f6d744787')
