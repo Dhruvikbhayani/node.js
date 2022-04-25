b77ebb417f882d9708022975006de4c6131ad8ea
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-04-25 10:32:47 +0530

    25-04

--- project/restfulapi/app.js
+++ project/restfulapi/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.post("/students", (req, res) => {
+    res.send('Hello')
+})
 
 app.listen(5050, () => {
     console.log("server is run 5050")
