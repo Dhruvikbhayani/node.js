ede78a714197d3222954861c30acaea60cc3a9b2
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-04-29 12:07:12 +0530

    29-04

--- project/api/app.js
+++ project/api/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+const student = require('./models/stdent')
 const mongoose = require('./db/cons')
 const port = process.env.Port || 2525
 
@@ -7,7 +9,18 @@ app.use(express.json())
 
 //create student
 app.post("/students", (req, res) => {
-    res.send('heloo')
+
+    const user = new student(req.body)
+
+    user.save().then((err, data) => {
+            if (err) throw err
+            else {
+                res.status(201).send(data)
+            }
+        })
+        // .catch((e) => {
+        //     res.status(400).send(e)
+        // })
 })
 app.listen(port, () => {
     console.log(`server is run ${port}`)
---
+++ project/api/models/stdent.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const studentschema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "Email id is already present"],
+        validate(val) {
+            if (!validator.isEmail(val)) {
+                throw new Error('Unvaid Email')
+
+            }
+        }
+    },
+    Phone: {
+        type: Number,
+        unique: true,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true,
+    }
+
+})
+
+//we will create model
+
+const student = new mongoose.model("students", studentschema)
+
+module.exports = student
