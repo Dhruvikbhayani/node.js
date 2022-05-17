289ccdd7c88d7b895100412c90bfbc4680aae8ae
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-05-17 10:34:18 +0530

    17-05

--- project/api/app.js
+++ project/api/app.js
@@ -3,6 +3,7 @@ const app = express()
 app.use(express.json())
 const student = require('./models/stdent')
 const mongoose = require('./db/cons')
+const async = require('hbs/lib/async')
 const port = process.env.Port || 2525
 
 app.use(express.json())
@@ -20,9 +21,46 @@ app.post("/students", (req, res) => {
         })
 })
 
-app.get("/get", (req, res) => {
-    student.find().then(data => res.send(data))
+//find student
+app.get("/get", async(req, res) => {
+    try {
+
+        await student.find().then(data => res.send(data))
+    } catch (e) {
+        res.send(e)
+    }
 })
+
+//delete student
+app.delete("/delete/:id", async(req, res) => {
+    try {
+        const id = req.params.id;
+        const deletedata = await student.findByIdAndDelete(id)
+        if (!deletedata) {
+            return res.status(404).send()
+        } else {
+
+            res.status(200).send(deletedata)
+        }
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+//update student
+app.patch("/update/:id", async(req, res) => {
+    try {
+        const id = req.params.id
+        const updatedata = await student.findByIdAndUpdate(id, req.body, { new: true })
+        res.status(200).send(updatedata)
+
+
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+
 
 
 app.listen(port, () => {
